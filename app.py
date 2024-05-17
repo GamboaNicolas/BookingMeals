dfc662f384aca0fd73914e55fbe658ffe93497fc
Nicolas_Notebook <gamboa.nicolas2002@gmail.com>
2024-05-17 18:36:02 -0300

    Agregué función reservar único en la app. CheckBox de ocultar ventana.

--- app.py
+++ app.py
@@ -1,56 +1,33 @@
 import customtkinter as ctk 
-import tkinter.messagebox as tkmb 
+import tkinter.messagebox as tkmb
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import getpass as getpass
-import pyautogui as gui
-
 import time as time
 
 options = Options()
-options.set_preference("detach", True)
-
-dia = "viernes"
-hora = "20:00"
-comedor = "Salud"
+# options.set_preference("detach", True)
+# options.experimental_options("detach", True)
+# dia = "viernes"
+# hora = "20:00"
+# comedor = "Salud"
 # Selecting GUI theme - dark, light , system (for system default) 
 ctk.set_appearance_mode("dark") 
 
 # Selecting color theme - blue, green, dark-blue 
-ctk.set_default_color_theme("blue") 
+ctk.set_default_color_theme("blue")
 
 app = ctk.CTk() 
-app.geometry("400x400") 
-app.title("Modern Login UI using Customtkinter") 
+app.geometry("800x400") 
+app.title("BookingMeals by Nicolas Gamboa")
+# def reservar_unico():
 
-
-def reservar(): 
-	
-    
-    dni = user_entry.get()
-    password = user_pass.get()
-
-    app.destroy()
-
-    driver = webdriver.Chrome(service = Service(GeckoDriverManager().install()), 
-                          options = options)
-    driver.get("https://comedores.unr.edu.ar/comedor-reserva/reservar")
-    driver.minimize_window()
-	
-    enter_username = driver.find_element("name", "form-login[dni]")
-    enter_password = driver.find_element("name", "form-login[clave]")
-    submit_keys = driver.find_element("name", "botones[botonEnviar]")
-    
-    enter_username.send_keys(dni)
-    enter_password.send_keys(password)
-    
-    submit_keys.click()
-    time.sleep(5)
-    salud = driver.find_elements("xpath",
+def reservar_unico(driver, comedor, dia, hora):
+    lugar = driver.find_elements("xpath",
                                  "//h2[@class='reservar-comedor-nombre' and contains(text(),'Comedor Universitario Área "+comedor+"')]")
-    driver.execute_script("arguments[0].click()", salud[-1])
+    driver.execute_script("arguments[0].click()", lugar[-1])
 
     horario_noche = []
     salir = []
@@ -94,20 +71,48 @@ def reservar():
 
 
 
+def reservar(): 
+	
+    dni = user_entry.get()
+    password = user_pass.get()
 
-label = ctk.CTkLabel(app,text="This is the main UI page") 
+  # app.destroy()
+    
+    if checkbox.get():
+        options.add_argument("--headless")
+    # print(ocultar)
+    driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), 
+                              options = options)
+    driver.minimize_window()
+    driver.get("https://comedores.unr.edu.ar/comedor-reserva/reservar")
+	
+    enter_username = driver.find_element("name", "form-login[dni]")
+    enter_password = driver.find_element("name", "form-login[clave]")
+    submit_keys = driver.find_element("name", "botones[botonEnviar]")
+    
+    enter_username.send_keys(dni)
+    enter_password.send_keys(password)
+    
+    submit_keys.click()
+    time.sleep(5)
+    reservar_unico(driver, "Salud", "viernes", "20:00")
 
-label.pack(pady=20) 
+
+
+
+# label = ctk.CTkLabel(app,text="") 
+
+# label.pack(pady=20) 
 
 
 frame = ctk.CTkFrame(master=app) 
 frame.pack(pady=20,padx=40,fill='both',expand=True) 
 
-label = ctk.CTkLabel(master=frame,text='Modern Login System UI') 
+label = ctk.CTkLabel(master=frame,text='BookingMeals') 
 label.pack(pady=12,padx=10) 
 
 
-user_entry= ctk.CTkEntry(master=frame,placeholder_text="Username") 
+user_entry= ctk.CTkEntry(master=frame,placeholder_text="DNI") 
 user_entry.pack(pady=12,padx=10) 
 
 user_pass= ctk.CTkEntry(master=frame,placeholder_text="Password",show="*") 
@@ -117,8 +122,7 @@ user_pass.pack(pady=12,padx=10)
 button = ctk.CTkButton(master=frame,text='Login',command=reservar) 
 button.pack(pady=12,padx=10) 
 
-checkbox = ctk.CTkCheckBox(master=frame,text='Remember Me') 
+checkbox = ctk.CTkCheckBox(master=frame,text='Ocultar navegador') 
 checkbox.pack(pady=12,padx=10) 
-
 
 app.mainloop()
